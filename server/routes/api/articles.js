bcbb7b75580180cf751c6e2b9105d8c451513b9e
John Eastwood <jjge732@gmail.com>
2020-08-25 10:53:36 -0500

    Made API more informative

--- server/routes/api/articles.js
+++ server/routes/api/articles.js
@@ -9,25 +9,35 @@ const controller = new Controller();
 router.get('/', async (req, res, next) => {
   const { page } = req.query;
   let { quantity } = req.query;
-  if (typeof (quantity) !== 'number') {
-    quantity = 1;
+  if (isNaN(quantity)) {
+    res.status(400).send('Quantity must be an integer.');
+  } else {
+    quantity = parseInt(quantity, 10);
   }
   await controller.NYT.getArticleMetaData(page, quantity)
-    .then((response) => res.json(response));
+    .then((response) => res.status(200).json(response));
   next();
 });
 
 router.get('/:source', async (req, res, next) => {
   const { page } = req.query;
+  const { source } = req.params;
   let { quantity } = req.query;
-  const sourceMap = {
-    nyt: controller.NYT,
-  };
-  if (typeof (quantity) !== 'number') {
-    quantity = 1;
+  const sourceMap = new Map([
+    ['nyt', controller.NYT],
+  ]);
+  if (isNaN(quantity)) {
+    res.status(400).send('Quantity must be an integer.');
+  } else if (sourceMap.has(source)) {
+    quantity = parseInt(quantity, 10);
+    await sourceMap.get(source).getArticleMetaData(page, quantity)
+      .then((response) => res.status(200).json(response));
+  } else {
+    res.status(400).json({
+      message: 'Unsupported source. Please send only one of the following sources',
+      sources: [...sourceMap.keys()],
+    });
   }
-  await sourceMap[req.params.source].getArticleMetaData(page, quantity)
-    .then((response) => res.json(response));
   next();
 });
 
